9d4ea19bf13bf53801080c097716756d7be78da8
Jenifer Soares <jey.souza11@gmail.com>
2020-11-09 17:00:47 -0300

    Cria Tela de Login/Registro

---
+++ Desafio_02_Android/app/src/main/java/com/example/myapplication/desafio_02_android/ListaRestaurante.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.desafio_02_android
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ListaRestaurante : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lista_restaurante, container, false)
+    }
+
+}
--- Desafio_02_Android/app/src/main/java/com/example/myapplication/desafio_02_android/RegisterActivity.kt
+++ Desafio_02_Android/app/src/main/java/com/example/myapplication/desafio_02_android/RegisterActivity.kt
@@ -2,11 +2,12 @@ package com.example.myapplication.desafio_02_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_register)
     }
+
 }
