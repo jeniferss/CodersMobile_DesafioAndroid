89d84704a4270e169d30a4a993c575cd72082f25
Jenifer Soares <jey.souza11@gmail.com>
2020-11-11 16:14:59 -0300

    Adiciona Login

--- Desafio_02_Android/app/src/main/java/com/example/myapplication/desafio_02_android/login/MainActivity.kt
+++ Desafio_02_Android/app/src/main/java/com/example/myapplication/desafio_02_android/login/MainActivity.kt
@@ -4,10 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import com.example.myapplication.desafio_02_android.R
 import com.example.myapplication.desafio_02_android.register.RegisterActivity
 import com.example.myapplication.desafio_02_android.home.HomeActivity
 import com.example.myapplication.desafio_02_android.menu.MenuActivity
+import com.example.myapplication.desafio_02_android.register.PROSSEGUIR
+import com.google.android.material.textfield.TextInputEditText
+
+const val ERRO_VAZIO = "CAMPO VAZIO"
+var PROSSEGUIR_L = false
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +26,38 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.btnLogin).setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+
+            val email = findViewById<TextInputEditText>(R.id.etEmail).text.toString()
+            val senha = findViewById<TextInputEditText>(R.id.etPassword).text.toString()
+
+            checarCampos(email, senha)
+
+            if (PROSSEGUIR_L) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                PROSSEGUIR_L = false
+                clear()
+            }
         }
+
     }
+
+    private fun checarCampos(email: String, senha: String) {
+
+        if (email.trim().isEmpty()) {
+            findViewById<TextInputEditText>(R.id.etEmail).error = ERRO_VAZIO
+            PROSSEGUIR_L = false
+        }else if (senha.trim().isEmpty()) {
+            findViewById<TextInputEditText>(R.id.etPassword).error = ERRO_VAZIO
+            PROSSEGUIR_L = false
+        } else PROSSEGUIR_L = true
+
+    }
+
+    private fun clear(){
+        findViewById<TextInputEditText>(R.id.etEmail).text!!.clear()
+        findViewById<TextInputEditText>(R.id.etPassword).text!!.clear()
+
+    }
+
 }
--- Desafio_02_Android/app/src/main/java/com/example/myapplication/desafio_02_android/register/RegisterActivity.kt
+++ Desafio_02_Android/app/src/main/java/com/example/myapplication/desafio_02_android/register/RegisterActivity.kt
@@ -4,19 +4,69 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import com.example.myapplication.desafio_02_android.R
 import com.example.myapplication.desafio_02_android.home.HomeActivity
+import com.example.myapplication.desafio_02_android.login.PROSSEGUIR_L
+import com.google.android.material.textfield.TextInputEditText
+
+const val ERRO_VAZIO = "CAMPO VAZIO"
+const val ERRO_SENHA = "SENHAS NÃO CONFEREM"
+var PROSSEGUIR = false
 
 class RegisterActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         findViewById<Button>(R.id.btnRegister).setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+
+            val name = findViewById<TextInputEditText>(R.id.etName).text.toString()
+            val email = findViewById<TextInputEditText>(R.id.etEmailRegister).text.toString()
+            val senha = findViewById<TextInputEditText>(R.id.etPasswordRegister).text.toString()
+            val senhaRepetir =
+                findViewById<TextInputEditText>(R.id.etPasswordRepeat).text.toString()
+
+            checarCampos(name, email, senha, senhaRepetir)
+
+            if (PROSSEGUIR) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                PROSSEGUIR = false
+                clear()
+            }
         }
 
     }
 
+    private fun checarCampos(name: String, email: String, senha: String, senhaRepetir: String) {
+
+        if (name.trim().isEmpty()) {
+            findViewById<TextInputEditText>(R.id.etName).error = ERRO_VAZIO
+            PROSSEGUIR = false
+        } else if (email.trim().isEmpty()) {
+            findViewById<TextInputEditText>(R.id.etEmailRegister).error = ERRO_VAZIO
+            PROSSEGUIR = false
+        } else if (senha.trim().isEmpty()) {
+            findViewById<TextInputEditText>(R.id.etPasswordRegister).error = ERRO_VAZIO
+            PROSSEGUIR = false
+        } else if (senhaRepetir.trim().isEmpty()) {
+            findViewById<TextInputEditText>(R.id.etPasswordRepeat).error = ERRO_VAZIO
+            PROSSEGUIR = false
+        } else if (senha != senhaRepetir) {
+            Toast.makeText(this@RegisterActivity, ERRO_SENHA, Toast.LENGTH_SHORT).show()
+            PROSSEGUIR = false
+        } else PROSSEGUIR = true
+    }
+
+    private fun clear() {
+        findViewById<TextInputEditText>(R.id.etName).text!!.clear()
+        findViewById<TextInputEditText>(R.id.etEmailRegister).text!!.clear()
+        findViewById<TextInputEditText>(R.id.etPasswordRegister).text!!.clear()
+        findViewById<TextInputEditText>(R.id.etPasswordRepeat).text!!.clear()
+    }
+
 }
+
